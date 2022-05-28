c2e8a6db6563ab68a724fa3a5245f0acea0a86be
canciyer <88708263+canciyer@users.noreply.github.com>
2022-05-28 19:46:24 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = ({
+	token: "5278187676:AAH2mae3ENdXlyE1ONLfUjK4isxrJDKmZyE",
+	botIsmi: "TeslaGameBot",
+	resmiKanal: "baxbura",
+	raundSayi: 3,
+	saniye: 5000,
+	emojiSaniye: 4,
+});
---
+++ functions.js
@@ -0,0 +1,29 @@
+module.exports = {
+	arrayRandom: arr => {
+		return arr[Math.round(Math.random() * (arr.length - 1))];
+	},
+	getUserLink: user => {
+	const lastName = user.last_name ? ` <b>${user.last_name}</b>` : '';
+	const username = user.username ? ` \n\n<b>👤Kullanıcı Adı: @${user.username}</b>` : '';
+	const userBio = user.bio ? ` \n\n<b>📖Bio: ${user.bio}</b>` : '';
+	return `<b>Ad: <a href="tg://user?id=${user.id}">${user.first_name}${lastName}</a></b>${username}${userBio}\n\n🆔: <code>${user.id}</code>`;
+    },
+    plusminus: n => n > 0 ? `+${n}` : n,
+	trueTrim: str => str.replace(/\t+/gm, ""),
+	pluralize: (n, singular, plural, accusative) => {
+		n = Math.abs(n)
+		let n10 = n % 10;
+		let n100 = n % 100;
+		if (n10 == 1 && n100 != 11) {
+			return singular;
+		}
+		if (
+			(2 <= n10 && n10 <= 4) &&
+			!(12 <= n100 && n100 <= 14)
+		) {
+			return plural;
+		}
+		return accusative;
+	}
+}
+ 
